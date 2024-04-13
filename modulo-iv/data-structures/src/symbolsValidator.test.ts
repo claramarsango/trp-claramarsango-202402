ca61bb6aaf1fd30b4806650062fa379e8b498064
claramarsango <claraxmarsango@gmail.com>
2024-04-13 17:01:29 +0200

    feat: add symbols validator function

--- modulo-iv/data-structures/src/symbolsValidator.test.ts
+++ modulo-iv/data-structures/src/symbolsValidator.test.ts
@@ -1,3 +1,5 @@
+import { validateSymbolCombination } from './symbolsValidator';
+
 describe('Given a function to validate a correct combination of symbols,', () => {
   test('when the function is called, it should be defined', () => {
     expect(typeof validateSymbolCombination).toBe('function');
