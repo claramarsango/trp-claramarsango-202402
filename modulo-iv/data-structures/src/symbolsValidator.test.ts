8cf56539e7a477cb880bf40a141ff63927e9bd0a
claramarsango <claraxmarsango@gmail.com>
2024-04-13 17:15:03 +0200

    refactor: define type of invalid argument

--- modulo-iv/data-structures/src/symbolsValidator.test.ts
+++ modulo-iv/data-structures/src/symbolsValidator.test.ts
@@ -6,7 +6,7 @@ describe('Given a function to validate a correct combination of symbols,', () =>
   });
 
   test('when the function receives an argument that is not a string, then it should return an error message', () => {
-    const invalidArgument = 1;
+    const invalidArgument = 1 as unknown as string;
     expect(validateSymbolCombination(invalidArgument)).toBe(
       'input must be a string',
     );
