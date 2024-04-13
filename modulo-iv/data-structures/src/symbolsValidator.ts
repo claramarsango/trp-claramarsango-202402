c13a9886f65cc250db9950da936c023eb756ed24
claramarsango <claraxmarsango@gmail.com>
2024-04-13 18:28:26 +0200

    feat: add only required symbols or empty string validation

--- modulo-iv/data-structures/src/symbolsValidator.ts
+++ modulo-iv/data-structures/src/symbolsValidator.ts
@@ -1,5 +1,16 @@
+const validateInputContent = (input: string): string | undefined => {
+  const validInputRegex = /^[()\[\]{}]*$/;
+
+  if (!validInputRegex.test(input)) {
+    return `input should either contain any of the following symbols: "(), [], {}" or be empty`;
+  }
+};
+
 export const validateSymbolCombination = (symbolCombination: string) => {
   if (typeof symbolCombination !== 'string') {
     return 'input must be a string';
   }
+
+  if (validateInputContent(symbolCombination))
+    return validateInputContent(symbolCombination);
 };
