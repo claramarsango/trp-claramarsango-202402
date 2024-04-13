622e087c5147bb3fba25e305a474e28bc4b8a79f
claramarsango <claraxmarsango@gmail.com>
2024-04-13 17:07:44 +0200

    test: add type of argument validation

--- modulo-iv/data-structures/src/symbolsValidator.test.ts
+++ modulo-iv/data-structures/src/symbolsValidator.test.ts
@@ -4,4 +4,11 @@ describe('Given a function to validate a correct combination of symbols,', () =>
   test('when the function is called, it should be defined', () => {
     expect(typeof validateSymbolCombination).toBe('function');
   });
+
+  test('when the function receives an argument that is not a string, then it should return an error message', () => {
+    const invalidArgument = 1;
+    expect(validateSymbolCombination(invalidArgument)).toBe(
+      'input must be a string',
+    );
+  });
 });
