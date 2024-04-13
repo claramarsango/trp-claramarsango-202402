cbebeb3df0ee58dc1a47c0f847f552ed21433b24
claramarsango <claraxmarsango@gmail.com>
2024-04-13 18:36:09 +0200

    refactor: extract input validation content logic

--- modulo-iv/data-structures/src/symbolsValidator.ts
+++ modulo-iv/data-structures/src/symbolsValidator.ts
@@ -1,16 +1,14 @@
 const validateInputContent = (input: string): string | undefined => {
   const validInputRegex = /^[()\[\]{}]*$/;
 
+  if (typeof input !== 'string') return 'input must be a string';
+
   if (!validInputRegex.test(input)) {
     return `input should either contain any of the following symbols: "(), [], {}" or be empty`;
   }
 };
 
 export const validateSymbolCombination = (symbolCombination: string) => {
-  if (typeof symbolCombination !== 'string') {
-    return 'input must be a string';
-  }
-
   if (validateInputContent(symbolCombination))
     return validateInputContent(symbolCombination);
 };
